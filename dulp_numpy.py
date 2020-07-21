d1f9adde75805861afeef65710bf952fcf80360e
Rupert Tombs <rt500@cam.ac.uk>
2020-07-21 21:30:47 +0100

    Implement numpy-style behavior with exceptions

--- dulp_numpy.py
+++ dulp_numpy.py
@@ -5,25 +5,75 @@ python -m timeit -vv -s "from dulp_numpy import perf" "perf()"
 
 """
 import numpy
-from numpy import uint64, int64, uint32, int32, float32
+from numpy import (
+    uint64, int64, uint32, int32,
+    float64, float32,
+)
 
-# TODO normal numpy behavior
-# TODO preamble and exceptions
-# TODO separate file
+# TODO tests
 
 def val(x):
-    u = x.view(uint64)
-    return -(u >> 63) ^ (u | 1 << 63)
+    x = numpy.asanyarray(x)
+
+    ftype = x.dtype.type
+    if ftype is float64:
+        utype = uint64
+        shift = utype(63)
+    elif ftype is float32:
+        utype = uint32
+        shift = utype(31)
+    else:
+        raise TypeError("must be float64 or float32, not %s" % x.dtype)
+
+    if x.ndim == 0:
+        x = ftype(x)
+
+    u = x.view(utype)
+    u = -(u >> shift) ^ (u | utype(1) << shift)
+
+    return u
 
 
 def dif(vx, vy):
-    hi = (vy >> 1).view(int64)
-    hi -= (vx >> 1).view(int64)
+    vx = numpy.asanyarray(vx)
+    vy = numpy.asanyarray(vy)
+    
+    if vx.shape != vy.shape:
+        shapes = (vx.shape, vy.shape)
+        raise ValueError("vy vy have different shapes %s %s" % shapes)
+    
+    if vx.dtype is not vy.dtype:
+        types = (vx.dtype, vy.dtype)
+        raise TypeError("vx vy have different types %s %s" % types)
+    
+    utype = vx.dtype.type
+    if utype is uint64:
+        itype = int64
+    elif utype is uint32:
+        itype = int32
+    else:
+        raise TypeError("must be uint64 or uint64, not %s" % vx.dtype)
+    
+    if vx.ndim == 0:
+        vx = utype(vx)
+        vy = utype(vy)
+
+    one = utype(1)
+    hi = (vy >> one).view(itype)
+    hi -= (vx >> one).view(itype)
     hi = hi.astype(float32)
-    lo = (vy & 1).astype(int32)
-    lo -= (vx & 1).astype(int32)
+    lo = (vy & one).astype(int32)
+    lo -= (vx & one).astype(int32)
     lo = lo.astype(float32)
+
     return lo + 2.*hi
+
+
+def dulp(x, y):
+    return dif(val(x), val(y))
+    
+    
+
 
 
 ntest = 1000*1000
@@ -32,11 +82,10 @@ testx = numpy.random.rand(ntest) - 0.5
 testy = numpy.random.rand(ntest) - 0.5
 
 def perf():
-    return dif(val(testx), val(testy))
+    return dulp(testx, testy)
 
 
 if __name__ == "__main__":
-    import numpy
     x = numpy.array([1., numpy.nan, 1.])
     y = numpy.array([1. + 2**-52, numpy.nan, 1.5])
     vx = val(x)
@@ -45,4 +94,7 @@ if __name__ == "__main__":
     print(vy)
     d = dif(vx, vy)
     print(d)
-    print(numpy.log2(d + 0.5) + 1) 
+    print(dulp(1., 2.))
+    print(dulp([[1.],[4.]], [[2.],[3.]]))
+    print(dif(uint32(1), uint64(2)))
+    
