40dcbe2a73cc21ef5222efd926b6916a6c3cfee9
Rupert Tombs <rt500@cam.ac.uk>
2020-07-25 19:25:17 +0100

    Get rid of _no0d. Duplication is better.

--- dulp_numpy.py
+++ dulp_numpy.py
@@ -5,60 +5,66 @@ python -m timeit -vv -s "from dulp_numpy import perf" "perf()"
 
 """
 import numpy
-from numpy import (
-    uint64, int64, uint32, int32,
-    float64, float32,
-)
+
+from numpy import asanyarray
+from numpy import float32, float64
+from numpy import int32, int64
+from numpy import uint32, uint64
 
 # TODO documentation
 # TODO tests
 
 def dulp(x, y):
-    x = numpy.asanyarray(x)
-    y = numpy.asanyarray(y)
+    x = asanyarray(x)
+    y = asanyarray(y)
 
     if x.dtype is not y.dtype:
         raise TypeError("%s is not %s" % (x.dtype, y.dtype))
 
     if x.dtype.type is float64:
-        cdulp = _dulp
+        r = _dulp(x, y)
     elif x.dtype.type is float32:
-        cdulp = _dulpf
+        r = _dulpf(x, y)
     else:
         raise TypeError("%s not in (float64, float32)" % x.dtype)
 
-    return _un0d(cdulp(x, y))
+    if r.ndim == 0:
+        r = r.dtype.type(x)
+    return r
 
 
 def val(x):
-    x = numpy.asanyarray(x)
+    x = asanyarray(x)
 
     if x.dtype.type is float64:
-        cval = _val
+        r = _val(x)
     elif x.dtype.type is float32:
-        cval = _valf
+        r = _valf(x)
     else:
         raise TypeError("%s not in (float64, float32)" % x.dtype)
 
-    return _un0d(cval(x))
+    if r.ndim == 0:
+        r = r.dtype.type(x)
+    return r
 
 
 def dif(vx, vy):
-    vx = numpy.asanyarray(vx)
-    vy = numpy.asanyarray(vy)
+    vx = asanyarray(vx)
+    vy = asanyarray(vy)
 
     if vx.dtype is not vy.dtype:
         raise TypeError("%s is not %s" % (vx.dtype, vy.dtype))
 
     if vx.dtype.type is uint64:
-        cdif = _dif
+        r = _dif(vx, vy)
     elif vx.dtype.type is uint32:
-        cdif = _diff
+        r = _diff(vx, vy)
     else:
         raise TypeError("%s not in (uint64, uint32)" % vx.dtype)
 
-    return _un0d(cdif(vx, vy))
-
+    if r.ndim == 0:
+        r = r.dtype.type(x)
+    return r
 
 
 def _dulp(x, y):
@@ -82,7 +88,7 @@ def _dif(vx, vy):
     hi -= (vx >> one).view(int64)
     hi = hi.astype(float32)
     lo = (vy & one).astype(int32)
-    lo -= (vx & one).astype(int32)
+    lo -= (vx & one).view(int64)
     lo = lo.astype(float32)
     return float32(2)*hi + lo
 
@@ -106,9 +112,3 @@ def _diff(vx, vy):
     r = vx.astype(int64)
     r -= vy.astype(int64)
     return r.astype(float32)
-
-
-def _un0d(x):
-    if x.ndim == 0:
-        x = x.dtype.type(x)
-    return x
