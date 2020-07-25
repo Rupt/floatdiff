3bacde398726d7cba533961a77167be681ec8f6d
Rupert Tombs <rt500@cam.ac.uk>
2020-07-25 19:24:50 +0100

    Remove long double speculation. Go back to anonymous unions.

--- dulp.c
+++ dulp.c
@@ -8,39 +8,14 @@ stdint.h
 
 */
 
-struct dulps80 {
-    uint64_t m;
-    uint16_t e;
-};
-
-
-union dulpw64 {
-    double f;
-    uint64_t u;
-};
-
-union dulpw32 {
-    float f;
-    uint32_t u;
-};
-
-union dulpw80 {
-    long double f;
-    struct dulps80 u;
-};
-
-
 float dulp(double x, double y);
 float dulpf(float x, float y);
-float dulpl(long double x, long double y);
 
 uint64_t dulpval(double x);
 uint32_t dulpvalf(float x);
-struct dulps80 dulpvall(long double x);
 
 float dulpdif(uint64_t vx, uint64_t vy);
 float dulpdiff(uint32_t vx, uint32_t vy);
-float dulpdifl(struct dulps80 vx, struct dulps80 vy);
 
 
 float
@@ -56,7 +31,7 @@ dulp(double x, double y)
 uint64_t
 dulpval(double x)
 {
-    union dulpw64 w;
+    union {double f; uint64_t u;} w;
     w.f = x;
     return -(w.u >> 63) ^ (w.u | (uint64_t)1 << 63);
 }
@@ -85,7 +60,7 @@ dulpf(float x, float y)
 uint32_t
 dulpvalf(float x)
 {
-    union dulpw32 w;
+    union {float f; uint32_t u;} w;
     w.f = x;
     return -(w.u >> 31) ^ (w.u | (uint32_t)1 << 31);
 }
@@ -95,28 +70,4 @@ float
 dulpdiff(uint32_t vx, uint32_t vy)
 {
     return (int64_t)vy - (int64_t)vx;
-}
-
-
-float
-dulpl(long double x, long double y)
-{
-    struct dulps80 vx, vy;
-    vx = dulpvall(x);
-    vy = dulpvall(y);
-    return dulpdifl(vx, vy);
-}
-
-
-struct dulps80
-dulpvall(long double x)
-{
-    return (struct dulps80){0, 0};
-}
-
-
-float
-dulpdifl(struct dulps80 vx, struct dulps80 vy)
-{
-    return 0.f;
 }
