eb33c3b5397f11d7eb312803b55067bc49f103f2
Rupert Tombs <rt500@cam.ac.uk>
2020-07-25 11:22:01 +0100

    Split into external/internal parts. Add un0d method to remove 0d arrays (yuck)

--- dulp_numpy.py
+++ dulp_numpy.py
@@ -13,24 +13,34 @@ from numpy import (
 # TODO documentation
 # TODO tests
 
-def val(x):
+def dulp(x, y):
     x = numpy.asanyarray(x)
+    y = numpy.asanyarray(y)
 
-    ftype = x.dtype.type
-    if ftype is float64:
-        utype = uint64
-        shift = utype(63)
-    elif ftype is float32:
-        utype = uint32
-        shift = utype(31)
+    if x.dtype is not y.dtype:
+        raise TypeError("%s is not %s" % (x.dtype, y.dtype))
+
+    if x.dtype.type is float64:
+        cdulp = _dulp
+    elif x.dtype.type is float32:
+        cdulp = _dulpf
     else:
         raise TypeError("%s not in (float64, float32)" % x.dtype)
 
-    if x.ndim == 0:
-        x = ftype(x)
+    return _un0d(cdulp(x, y))
 
-    u = x.view(utype)
-    return -(u >> shift) ^ (u | utype(1) << shift)
+
+def val(x):
+    x = numpy.asanyarray(x)
+
+    if x.dtype.type is float64:
+        cval = _val
+    elif x.dtype.type is float32:
+        cval = _valf
+    else:
+        raise TypeError("%s not in (float64, float32)" % x.dtype)
+
+    return _un0d(cval(x))
 
 
 def dif(vx, vy):
@@ -40,29 +50,65 @@ def dif(vx, vy):
     if vx.dtype is not vy.dtype:
         raise TypeError("%s is not %s" % (vx.dtype, vy.dtype))
 
-    utype = vx.dtype.type
-    if utype is uint64:
-        itype = int64
-    elif utype is uint32:
-        itype = int32
+    if vx.dtype.type is uint64:
+        cdif = _dif
+    elif vx.dtype.type is uint32:
+        cdif = _diff
     else:
         raise TypeError("%s not in (uint64, uint32)" % vx.dtype)
 
-    if vx.ndim == 0:
-        vx = utype(vx)
-        vy = utype(vy)
+    return _un0d(cdif(vx, vy)
 
-    one = utype(1)
-    hi = (vy >> one).view(itype)
-    hi -= (vx >> one).view(itype)
+
+
+def _dulp(x, y):
+    vx = _val(x)
+    vy = _val(y)
+    return _dif(vx, vy)
+
+
+def _val(x):
+    u = x.view(int64)
+    shift = int64(63)
+    r = (u >> shift)
+    r |= (int64(1) << shift)
+    r ^= u
+    return r.view(uint64)
+
+
+def _dif(vx, vy):
+    one = uint64(1)
+    hi = (vy >> one).view(int64)
+    hi -= (vx >> one).view(int64)
     hi = hi.astype(float32)
     lo = (vy & one).astype(int32)
     lo -= (vx & one).astype(int32)
     lo = lo.astype(float32)
-    return lo + 2.*hi
+    return float32(2)*hi + lo
 
 
-def dulp(x, y):
-    vx = val(x)
-    vy = val(y)
-    return dif(vx, vy)
+def _dulpf(x, y):
+    vx = _valf(x)
+    vy = _valf(y)
+    return _diff(vx, vy)
+
+
+def _valf(x):
+    u = x.view(int32)
+    shift = int32(31)
+    r = (u >> shift)
+    r |= (int32(1) << shift)
+    r ^= u
+    return r.view(uint32)
+
+
+def _diff(vx, vy):
+    r = vx.astype(int64)
+    r -= vy.astype(int64)
+    return r.astype(float32)
+
+
+def _un0d(x):
+    if x.ndim == 0:
+        x = x.dtype.type(x)
+    return x
