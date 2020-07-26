82911d37bbb1d5b933e6498197fad11341989da6
Rupert Tombs <rt500@cam.ac.uk>
2020-07-26 14:30:52 +0100

    Reorder definitions for consistency with declarations and numpy

--- dulp.c
+++ dulp.c
@@ -8,6 +8,7 @@ stdint.h
 
 */
 
+
 float dulp(double x, double y);
 float dulpf(float x, float y);
 
@@ -28,25 +29,6 @@ dulp(double x, double y)
 }
 
 
-uint64_t
-dulpval(double x)
-{
-    union {double f; uint64_t u;} w;
-    w.f = x;
-    return -(w.u >> 63) ^ (w.u | (uint64_t)1 << 63);
-}
-
-
-float
-dulpdif(uint64_t vx, uint64_t vy)
-{
-    float lo, hi;
-    hi = (int64_t)(vy >> 1) - (int64_t)(vx >> 1);
-    lo = (int)(vy & 1) - (int)(vx & 1);
-    return hi + hi + lo;
-}
-
-
 float
 dulpf(float x, float y)
 {
@@ -57,12 +39,31 @@ dulpf(float x, float y)
 }
 
 
+uint64_t
+dulpval(double x)
+{
+    union {double f; uint64_t u;} w;
+    w.f = x;
+    return -(w.u >> 63) ^ (w.u | (uint64_t)1 << 63);
+}
+
+
 uint32_t
 dulpvalf(float x)
 {
     union {float f; uint32_t u;} w;
     w.f = x;
     return -(w.u >> 31) ^ (w.u | (uint32_t)1 << 31);
+}
+
+
+float
+dulpdif(uint64_t vx, uint64_t vy)
+{
+    float lo, hi;
+    hi = (int64_t)(vy >> 1) - (int64_t)(vx >> 1);
+    lo = (int)(vy & 1) - (int)(vx & 1);
+    return hi + hi + lo;
 }
 
 
