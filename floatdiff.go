5c3549f37a309e60d14988361124b790f49007da
Rupert Tombs <rt500@cam.ac.uk>
2021-08-08 18:18:21 +0100

    use arithmetic right shift to replace bool hack

--- floatdiff.go
+++ floatdiff.go
@@ -20,20 +20,19 @@ func Floatdiff(x, y float64) float64 {
 func Rank(x float64) int64 {
 	const mask = (1 << 63) - 1
 	ibits := int64(math.Float64bits(x))
-	return -int64(ibool(ibits < 0)) ^ (ibits & mask)
+	return (ibits >> 63) ^ (ibits & mask)
 }
 
 func Rank32(x float32) int32 {
 	const mask = (1 << 31) - 1
 	ibits := int32(math.Float32bits(x))
-	return -int32(ibool(ibits < 0)) ^ (ibits & mask)
+	return (ibits >> 31) ^ (ibits & mask)
 }
 
 func Diff(valx, valy int64) float64 {
 	const shift = 32
 	const scale = 1 << 32
 	const mask = (1 << 32) - 1
-
 	hi := (valx >> shift) - (valy >> shift)
 	lo := (valy & mask) - (valx & mask)
 	return scale*float64(hi) + float64(lo)
@@ -41,11 +40,4 @@ func Diff(valx, valy int64) float64 {
 
 func Diff32(valx, valy int32) float64 {
 	return float64(int64(valy) - int64(valx))
-}
-
-func ibool(b bool) (i int) {
-	if b {
-		i++
-	}
-	return
 }
